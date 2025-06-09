5770708406429eb6779a683cd1daf67da8c89c23
ashok <ashoksakuru252@gmail.com>
2025-06-10 03:12:22 +0530

    new change

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,22 +1,27 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
-import { NextRequest } from 'next/server'
 export async function POST(req) {
   try {
-    const evt = await verifyWebhook(req)
-    const { id } = evt.data
-    const eventType = evt.type
+    const rawBody = await req.text(); 
+    const evt = await verifyWebhook({
+      req,
+      rawBody,
+    });
+    const eventType = evt.type;
+    if (eventType === 'session.created') {
+      console.log(`User signed in: ${evt.data.user_id}`);
+    }
     if (eventType === 'user.created') {
-        console.log("user created")
+      console.log("user created");
     }
     if (eventType === 'user.deleted') {
-        console.log("user deleted")
+      console.log("user deleted");
     }
     if (eventType === 'user.updated') {
-        console.log("user updated")
+      console.log("user updated");
     }
-    return new Response('Webhook received', { status: 200 })
+    return new Response('Webhook received', { status: 200 });
   } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return new Response('Error verifying webhook', { status: 400 })
+    console.error('Error verifying webhook:', err);
+    return new Response('Error verifying webhook', { status: 400 });
   }
 }
